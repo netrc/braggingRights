50b4c690eedffd2cff7df01c12f6a8b8b85a2ee2
netrc <ric@netrc.com>
2015-06-19 16:26:43 -0400

    adding PerSportsTotals to get rid of other globals

--- README.md
+++ README.md
@@ -11,6 +11,8 @@ Tracking North American cities cumulative sports championships...
 
 
 # Done
+* split city data mgmt to braggingRights.js
+* added mocha test
 * Add US map
 ** https://github.com/markmarkoh/datamaps/blob/master/README.md#using-custom-maps
 ** http://www.findlatitudeandlongitude.com/batch-geocode/
--- braggingRights.js
+++ braggingRights.js
@@ -1,22 +1,56 @@
 
 "use strict";
 
-/// class  to hold cities
+// class to hold a city's w/l record per sport (incl "all")
+var PerSportWinsLosses = function () {
+    var _wins = {'all':0};
+    var _losses = {'all':0};
+    var _wlArray = [];  // used in the popup grid
+
+    this.addWin = function (sp,y,w,l) {
+      if (! _wins[sp]) {
+        _wins[sp]=0;
+      }
+      _wins[sp]++;
+      _wins['all']++;
+		  _wlArray.push( [ y, sp, "Won", w, l ] );
+    };
+    this.addLoss = function (sp,y,w,l) {
+      if (! _losses[sp]) {
+        _losses[sp]=0;
+      }
+      _losses[sp]++;
+      _losses['all']++;
+		  _wlArray.push( [ y, sp, "Lost", w, l ] );
+    };
+
+    this.wins = function (sp) {
+      return ( _wins[sp]) ?  _wins[sp] : 0;
+    };
+    this.losses = function (sp) {
+      return ( _losses[sp]) ?  _losses[sp] : 0;
+    };
+    this.wlArray = function () {
+      return _wlArray;
+    };
+};
+
+/// class  to hold cities and their per-sport+all wins
 var CityData = function () {
 
     var _cityDataArray = [];
     var _cityDataObject = {};
     var _teamCityMap = {};
+    var _sportTotals = [];
 
-    this.addTeamCity = function (tc) {    // get an obj with team, city
-        //for (c in cityData.asList()) { console.log("c: "+c+" "); }
+    this.addTeamCity = function (tc) {    // tc an obj with team, city props
         _teamCityMap[tc.team] = tc.city;
         if ( !_cityDataObject[tc.city] ) {
-            _cityDataObject[tc.city] = 1; // props in obj; xxx - just one city
+            _cityDataObject[tc.city] = 1; // props in obj; uniqifies cities
             _cityDataArray.push(tc.city);
+            _sportTotals[tc.city] = new PerSportWinsLosses();
         }
     };
-
     this.asArray = function () {
         return _cityDataArray;
     };
@@ -31,6 +65,11 @@ var CityData = function () {
         }
         return _teamCityMap[t];
     };
+
+    this.sportTotals = function ( c ) {
+        return _sportTotals[c];
+    };
+
 };
 
 
@@ -39,28 +78,69 @@ var assert = require("assert")
 describe('CityData', function(){
   describe('addTeamCity', function(){
     it('one insert works', function(){
-      var tcd = new CityData();
-      tcd.addTeamCity( {team:'Tigers', city:'Detroit'} );
-      assert.deepEqual(tcd.asList(), {'Detroit':1});
-      assert.equal(tcd.teamToCity('Tigers'), 'Detroit');
-      assert.equal(tcd.asArray().length, 1);
+      var cd = new CityData();
+      cd.addTeamCity( {team:'Tigers', city:'Detroit'} );
+      assert.deepEqual(cd.asList(), {'Detroit':1});
+      assert.equal(cd.teamToCity('Tigers'), 'Detroit');
+      assert.equal(cd.asArray().length, 1);
     })
     it('duplicate insert works', function(){
-      var tcd = new CityData();
-      tcd.addTeamCity( {team:'Tigers', city:'Detroit'} );
-      tcd.addTeamCity( {team:'Lions', city:'Detroit'} );
-      assert.deepEqual(tcd.asList(), {'Detroit':1});
-      assert.equal(tcd.teamToCity('Tigers'), 'Detroit');
-      assert.equal(tcd.asArray().length, 1);
+      var cd = new CityData();
+      cd.addTeamCity( {team:'Tigers', city:'Detroit'} );
+      cd.addTeamCity( {team:'Lions', city:'Detroit'} );
+      assert.deepEqual(cd.asList(), {'Detroit':1});
+      assert.equal(cd.teamToCity('Tigers'), 'Detroit');
+      assert.equal(cd.asArray().length, 1);
     })
     it('two inserts work', function(){
-      var tcd = new CityData();
-      tcd.addTeamCity( {team:'Tigers', city:'Detroit'} );
-      tcd.addTeamCity( {team:'Yankees', city:'New York'} );
-      assert.deepEqual(tcd.asList(), {'Detroit':1, 'New York':1});
-      assert.equal(tcd.teamToCity('Tigers'), 'Detroit');
-      assert.equal(tcd.teamToCity('Yankees'), 'New York');
-      assert.equal(tcd.asArray().length, 2);
+      var cd = new CityData();
+      cd.addTeamCity( {team:'Tigers', city:'Detroit'} );
+      cd.addTeamCity( {team:'Yankees', city:'New York'} );
+      assert.deepEqual(cd.asList(), {'Detroit':1, 'New York':1});
+      assert.equal(cd.teamToCity('Tigers'), 'Detroit');
+      assert.equal(cd.teamToCity('Yankees'), 'New York');
+      assert.equal(cd.asArray().length, 2);
+    })
+  })
+  describe('perSportWinsLosses', function(){
+    it('one win works', function(){
+      var cd = new CityData();
+      cd.addTeamCity( {team:'Tigers', city:'Detroit'} );
+      cd.sportTotals('Detroit').addWin('mlb');
+      assert.equal(cd.sportTotals('Detroit').wins('mlb'),1);
+      assert.equal(cd.sportTotals('Detroit').losses('mlb'),0);
+      assert.equal(cd.sportTotals('Detroit').wins('all'),1);
+      assert.equal(cd.sportTotals('Detroit').losses('all'),0);
+    })
+    it('3w/2l works', function(){
+      var cd = new CityData();
+      cd.addTeamCity( {team:'Tigers', city:'Detroit'} );
+      cd.sportTotals('Detroit').addWin('mlb');
+      cd.sportTotals('Detroit').addWin('mlb');
+      cd.sportTotals('Detroit').addWin('mlb');
+      cd.sportTotals('Detroit').addLoss('mlb');
+      cd.sportTotals('Detroit').addLoss('mlb');
+      assert.equal(cd.sportTotals('Detroit').wins('mlb'),3);
+      assert.equal(cd.sportTotals('Detroit').losses('mlb'),2);
+      assert.equal(cd.sportTotals('Detroit').wins('all'),3);
+      assert.equal(cd.sportTotals('Detroit').losses('all'),2);
+    })
+    it('3 sports works', function(){
+      var cd = new CityData();
+      cd.addTeamCity( {team:'Tigers', city:'Detroit'} );
+      cd.sportTotals('Detroit').addWin('mlb');
+      cd.sportTotals('Detroit').addWin('mlb');
+      cd.sportTotals('Detroit').addLoss('mlb');
+      cd.sportTotals('Detroit').addWin('nfl');
+      cd.sportTotals('Detroit').addLoss('nhl');
+      assert.equal(cd.sportTotals('Detroit').wins('mlb'),2);
+      assert.equal(cd.sportTotals('Detroit').losses('mlb'),1);
+      assert.equal(cd.sportTotals('Detroit').wins('nfl'),1);
+      assert.equal(cd.sportTotals('Detroit').losses('nfl'),0);
+      assert.equal(cd.sportTotals('Detroit').wins('nhl'),0);
+      assert.equal(cd.sportTotals('Detroit').losses('nhl'),1);
+      assert.equal(cd.sportTotals('Detroit').wins('all'),3);
+      assert.equal(cd.sportTotals('Detroit').losses('all'),2);
     })
   })
 })
